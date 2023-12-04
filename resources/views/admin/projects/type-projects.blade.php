1c51785c9a221e008e7102b03a0975ba490fc9fa
Vahidone <v.ban.roma@gmail.com>
2023-12-04 22:24:35 +0100

    feat: put accordion for list of projects for type in type-projects.blade.php

--- resources/views/admin/projects/type-projects.blade.php
+++ resources/views/admin/projects/type-projects.blade.php
@@ -1,65 +1,38 @@
-
 @extends('layouts.admin')
-
 
 @section('content')
 
-
     @if(session('success'))
-
         <div class="alert alert-success" role="alert">
-
             {{ session('success')}}
-
         </div>
     @endif
 
     <div class="main-csm">
-        <h1 class="text-white mb-5">Projects & Types List</h1>
+        <h1 class="text-white mb-5">Projects for Type List</h1>
 
-        <table class="table table-dark table-striped text-white text-center">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Types</th>
-                <th scope="col">Project in relation</th>
-
-
-              </tr>
-            </thead>
-            <tbody>
-                @foreach ($types as $type)
-
-                <tr>
-                    <td>{{ $type->id}}</td>
-                    <td>{{ $type->title}}</td>
-                    <td>
-
-                        <ul class="list-group">
-                            @foreach ($type->projects as $project)
-
-                            @endforeach
-                            <li class="list-group-item bg-transparent border-0">
-                                <a class="link-type-projects" href="{{ route('admin.projects.show', $project)}}">{{ $project->title }}</a>
-                            </li>
-
-                        </ul>
-
-
-                    </td>
-
-
-                </tr>
-
-
-
-                @endforeach
-
-
-            </tbody>
-          </table>
-
+        <div class="accordion" id="accordionExample">
+            @foreach ($types as $type)
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-heading-{{ $type->id }}">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-{{ $type->id }}" aria-expanded="true" aria-controls="collapse-{{ $type->id }}">
+                            {{ $type->title}}
+                        </button>
+                    </h2>
+                    <div id="collapse-{{ $type->id }}" class="accordion-collapse collapse" aria-labelledby="flush-heading-{{ $type->id }}" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <ul class="list-group">
+                                @foreach ($type->projects as $project)
+                                    <li class="list-group-item bg-transparent border-0">
+                                        <a class="link-type-projects" href="{{ route('admin.projects.show', $project)}}">{{ $project->title }} <i class="fa-solid fa-up-right-from-square"></i></a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
-
 
 @endsection
